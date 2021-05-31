9d670a0f1a0663566b121e51d75454bbbb008beb
Jeremy Dowdall <jeremyd@aspencloud.com>
2021-05-31 09:24:45 -0400

    include basic server in server_html

--- oobium_server/lib/oobium_server_html.dart
+++ oobium_server/lib/oobium_server_html.dart
@@ -1,3 +1,5 @@
 library oobium_server;
 
+export 'package:oobium_server/src/server.dart';
+export 'package:oobium_server/src/server_settings.dart';
 export 'package:oobium_server/src/html/html.dart';
